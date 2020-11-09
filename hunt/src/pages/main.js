c23f271cd014b07b063a7f145229f093193e88bb
Mikael Josias Rodrigues <mikaeljosias2002@gmail.com>
2020-11-08 22:16:14 -0300

    8.Product List

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 
 export default class Main extends Component {
@@ -21,12 +21,24 @@ export default class Main extends Component {
         const { docs } = response.data;
 
         this.setState({ docs })
-    }
+    };
+
+    renderItem = ({ item }) => (
+    <View>
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+
+        <TouchableOpacity onPress={() => {}}><Text>Acessar</Text></TouchableOpacity>
+    </View>);
 
     render(){
         return (
             <View>
-                <Text>Página Main: {this.state.docs.map(product => <Text key={product._id}>{product.title}</Text>)}</Text>
+                <FlatList 
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         )
     }
