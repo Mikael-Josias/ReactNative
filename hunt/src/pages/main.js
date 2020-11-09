140dc45c3aa672a1bb680941123fae327a36ffd2
Mikael Josias Rodrigues <mikaeljosias2002@gmail.com>
2020-11-08 22:08:31 -0300

    7.State

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -7,6 +7,10 @@ export default class Main extends Component {
         title: "JSHunt"
     };
 
+    state = {
+        docs: [],
+    };
+
     componentDidMount(){
         this.loadProducts();
     }
@@ -16,13 +20,13 @@ export default class Main extends Component {
     
         const { docs } = response.data;
 
-        console.log(docs);
+        this.setState({ docs })
     }
 
     render(){
         return (
             <View>
-                <Text>Página Main</Text>
+                <Text>Página Main: {this.state.docs.map(product => <Text key={product._id}>{product.title}</Text>)}</Text>
             </View>
         )
     }
